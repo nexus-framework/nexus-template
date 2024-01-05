ed8879802ac31f334d3db9277744403a048c5ffa
afroze9 <afrozeamjad@gmail.com>
2024-01-05 15:05:06 +0500

    Modifications for library updates

--- ServiceTemplate/ServiceBootstrapper.cs
+++ ServiceTemplate/ServiceBootstrapper.cs
@@ -11,50 +11,16 @@ using {{RootNamespace}}.Telemetry;
 
 namespace {{RootNamespace}};
 
-public class ServiceBootstrapper : Bootstrapper
+public class ServiceBootstrapper : NexusServiceBootstrapper
 {
     public ServiceBootstrapper(string[] args) : base(args)
     {
     }
 
-    protected override void AddServices()
-    {
-        base.AddServices();
-        // Internal Services
-        AppBuilder.Services.AddSingleton<IPeopleInstrumentation, PeopleInstrumentation>();
-        
-        // Custom Meter for Metrics
-        AppBuilder.Services.AddOpenTelemetry()
-            .ConfigureResource(c =>
-            {
-                c.AddService("{{ServiceNameKebabCaseAndApi}}");
-            })
-            .WithMetrics(builder =>
-            {
-                builder.AddMeter(PeopleInstrumentation.MeterName);
-            });
-        
-        AppBuilder.Services.AddScoped<IPeopleService, PeopleService>();
-
-        // Libraries
-        AppBuilder.Services.AddAutoMapper(typeof(PeopleProfile));
-        AppBuilder.Services.AddValidatorsFromAssemblyContaining(typeof(Program));
-
-        // Persistence
-        AppBuilder.Services.AddCorePersistence<ApplicationDbContext>(AppBuilder.Configuration);
-        AppBuilder.Services.AddScoped<PeopleRepository>();
-        AppBuilder.Services.AddScoped<UnitOfWork>();
-    }
-
+   
     protected override void ConfigureMiddleware()
     {
         base.ConfigureMiddleware();
-        
-        // DB Migration
-        using IServiceScope scope = App.Services.CreateScope();
-        ApplicationDbContext db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        db.Database.Migrate();
-
         App.MapControllers();
     }
 }
